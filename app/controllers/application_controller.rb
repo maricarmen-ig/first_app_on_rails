8d5b1fe341fc4114e797adc3b38d20a9ee6b8ed1
maricarmen <igmaricarmenorozco7@gmail.com>
2025-09-24 23:52:12 -0600

    Agregando Presentacion en application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def hola
-    render html: "hola, mundo!"
+  def presentacion
+    render html: "hola, Somos Maricarmen y Samari, y estamos usando Ruby con Rails"
   end
 end
